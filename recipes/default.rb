467d0ffe33a0fb02155c743268f66851008d02bf
holy <holysugar@gmail.com>
2013-08-15 11:54:59 +0900

    simplify default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,58 +7,41 @@
 # MIT License
 #
 
-file_path = "#{Chef::Config[:file_cache_path]}/authorized_keys"
-
-b = bash "concat authorized_keys" do
-  cwd Chef::Config[:file_cache_path]
-
-  code <<-EOC
-    cat *.authorized_keys > authorized_keys
-  EOC
-
-  action :nothing
-
-  not_if { File.exists? file_path }
+node["github-pubkey"]["github-members"].each do |name|
+  remote_file "#{Chef::Config[:file_cache_path]}/#{name}.authorized_keys" do
+    source "https://github.com/#{name}.keys"
+    action :create
+  end
 end
 
-# copy authorized_keys to users
-node["github-pubkey"]["usernames"].each do |username|
+bash "concat authorized_keys" do
+  cwd Chef::Config[:file_cache_path]
+  code %{ cat *.authorized_keys > authorized_keys }
+end
 
-  d = directory "/home/#{username}/.ssh" do
+# all .ssh directory existance
+node["github-pubkey"]["usernames"].each do |username|
+  directory "/home/#{username}/.ssh" do
     owner  username
     group  username
     mode   0700
-    action :nothing
-
-    not_if { File.exists? "/home/#{username}/.ssh" }
   end
+end
 
-  f = file "/home/#{username}/.ssh/authorized_keys" do
-    owner   username
-    group   username
-    mode    0600
-    action  :nothing
-  end
+all_key_file = "#{Chef::Config[:file_cache_path]}/authorized_keys"
 
-  cp = bash "copy authorized_keys" do
+node["github-pubkey"]["usernames"].each do |username|
+  authorized_keys = "/home/#{username}/.ssh/authorized_keys"
+
+  bash "copy authorized_keys" do
     cwd Chef::Config[:file_cache_path]
-
-    code <<-EOC
-      cp "#{Chef::Config[:file_cache_path]}/authorized_keys" /home/#{username}/.ssh/authorized_keys
-    EOC
-
-    action :nothing
+    code   %{ cp   "#{all_key_file}" "#{authorized_keys}" }
+    not_if %{ diff "#{all_key_file}" "#{authorized_keys}" }
   end
 
-  node["github-pubkey"]["github-members"].each do |name|
-    r = remote_file "#{Chef::Config[:file_cache_path]}/#{name}.authorized_keys" do
-      source "https://github.com/#{name}.keys"
-      action :create
-    end
+  file authorized_keys do
+    owner username
+    group username
+    mode 0600
   end
-
-  b.run_action(:run)
-  d.run_action(:create)
-  cp.run_action(:run)
-  f.run_action(:create)
 end
